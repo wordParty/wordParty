31dc8d14703274d16a08630e34377c661d6d2a3f
aprillebalsom <aprillebalsom@gmail.com>
2020-10-02 16:55:32 -0400

    installed font-awesome + added firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const trashCan = <FontAwesomeIcon icon={faTrash} />;
+const exit = <FontAwesomeIcon icon={faTimes} />;
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+			<div>
+		
+			</div>
+		);
   }
 }
 
