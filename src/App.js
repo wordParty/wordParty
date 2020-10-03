d7fadd029e99a8925a4484d8ada82ae096c2d30f
aprillebalsom <aprillebalsom@gmail.com>
2020-10-03 10:27:39 -0400

    added toggle display package

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
+// import firebase from './firebase';
+import ToggleDisplay from 'react-toggle-display';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faTimes} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
+//FONT AWESOME ICONS
 const trashCan = <FontAwesomeIcon icon={faTrash} />;
 const exit = <FontAwesomeIcon icon={faTimes} />;
 
-export class App extends Component {
-  render() {
-    return (
-			<div>
-      <button>rendered word</button>
-			</div>
-		);
-  }
+class App extends Component {
+	render() {
+		return (
+      <div>
+        
+      </div>
+
+		)
+	}
 }
 
 export default App;
