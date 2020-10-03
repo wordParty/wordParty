e96ed3ce51edfe93c817c9dd76d4c4b6351ffb48
Thomas <t.garrison333@gmail.com>
2020-10-03 12:11:57 -0400

    api pull functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,93 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      rhymeInput: "",
+      synInput: "",
+      words: [],
+    };
+  }
+
+  getRhy = () => {
+    axios({
+      url: "https://api.datamuse.com/words",
+      params: {
+        max: 10,
+        rel_rhy: this.state.rhymeInput,
+      },
+    }).then((response) => {
+      let wordsResults = response.data;
+      this.setState({
+        words: wordsResults,
+      });
+    });
+  };
+
+  getSyn = () => {
+    axios({
+      url: "https://api.datamuse.com/words",
+      params: {
+        max: 10,
+        ml: this.state.synInput,
+      },
+    }).then((response) => {
+      let wordsResults = response.data;
+      this.setState({
+        words: wordsResults,
+      });
+    });
+  };
+
+  handleRhy = () => {
+    if (true) {
+      this.setState(
+        {
+          rhymeInput: "happy",
+          title: "Rhyme test"
+        },
+        () => {
+          this.getRhy();
+        }
+      );
+    }
+  };
+
+  handleSyn = () => {
+    if (true) {
+      this.setState(
+        {
+          synInput: "test",
+          title: "Synonym test",
+        },
+        () => {
+          this.getSyn();
+        }
+      );
+    }
+  };
+
   render() {
     return (
-      <div>
+      <div className="App wrapper">
+        <button onClick={() => this.handleSyn()}>Synonyms</button>
+        <button onClick={() => this.handleRhy()}>Rhymes</button>
+        <h1>{this.state.title}</h1>
+        {this.state.words.map((singleWord) => {
+          return (
+            <div key={singleWord.score} className="wordContainer">
+              <p onClick={alert('whoot!')}>{singleWord.word}</p>
+            </div>
+          );
+        })}
         
       </div>
-    )
+    );
   }
 }
 
-export default App
-
+export default App;
