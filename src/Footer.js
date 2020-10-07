e061bc2bc0f6227c12480fe813eb62b301fd73e7
aprillebalsom <aprillebalsom@gmail.com>
2020-10-07 13:28:35 -0400

    tweaked the lists to position button

--- src/Footer.js
+++ src/Footer.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react'
 class Footer extends Component {
     render() {
         return (
-            <div className="footer">
+            <footer>
                 <p>
-                    Created at <a href="https://junocollege.com/">Juno College</a> by:
+                    Created at <a href='https://junocollege.com/'>Juno College</a> by:
                 </p>
                 <p>
-                    <a href="https://github.com/thomasDotOnline">Thomas</a>
-                    , <a href="https://github.com/bksokhi">Baljit</a>
-                    , <a href="https://github.com/hshaikhnbake">Haris</a>,
-                    and <a href="https://github.com/aprillebalsom">Aprille</a>
+                    <a href='https://github.com/thomasDotOnline'>Thomas</a>,{' '}
+                    <a href='https://github.com/bksokhi'>Baljit</a>,{' '}
+                    <a href='https://github.com/hshaikhnbake'>Haris</a>, and{' '}
+                    <a href='https://github.com/aprillebalsom'>Aprille</a>
                 </p>
-            </div>
+            </footer>
         );
     }
 }
--- src/List.js
+++ src/List.js
@@ -7,25 +7,27 @@ const exit = <FontAwesomeIcon icon={faTimes} />;
 
 class List extends React.Component {
 
-        callRemove = () => {
-            this.props.removeHandle(this.props.title)
-        }
+    callRemove = () => {
+        this.props.removeHandle(this.props.title)
+    }
+
     render() {
         return (
-            
             <li key={this.props.listKey}>
-                <button className="removeList" title='remove' onClick={this.callRemove}>
-                <span className='srOnly'>
-                        Delete this list by clicking here.
-				        </span>{exit}</button>
-                
-                <h3>{this.props.title}</h3>
 
-                
+                <h3>{this.props.title}</h3>
                 {this.props.list}
-                
+                <button
+                    className='removeList'
+                    title='remove'
+                    onClick={this.callRemove}
+                >
+                    <span className='srOnly'>Delete this list by clicking here.</span>
+                    {exit}
+                </button>
+
             </li>
-        )
+		);
     }
 }
 
