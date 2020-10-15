6e19100ca7a069953448c99ebcf8e94232d70fde
aprillebalsom <aprillebalsom@gmail.com>
2020-10-15 11:19:45 -0400

    Updates

---
+++ precache-manifest.80fbd121a810c51718bcffef8f72720c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8949c0dc4ded14b064762119199ff62",
+    "url": "/wordParty/index.html"
+  },
+  {
+    "revision": "c01b8c952aa5d9c18105",
+    "url": "/wordParty/static/css/main.8a145800.chunk.css"
+  },
+  {
+    "revision": "119bfb836a87a09a953f",
+    "url": "/wordParty/static/js/2.35a70982.chunk.js"
+  },
+  {
+    "revision": "16056f19fc2a7e8fef3e22f99eb947d7",
+    "url": "/wordParty/static/js/2.35a70982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01b8c952aa5d9c18105",
+    "url": "/wordParty/static/js/main.39ee39b4.chunk.js"
+  },
+  {
+    "revision": "cccdb568bd443e2cf2ef",
+    "url": "/wordParty/static/js/runtime-main.a811ee6d.js"
+  },
+  {
+    "revision": "a16074b58d90392e499a3e961a577ed3",
+    "url": "/wordParty/static/media/looseLeafBckgnd.a16074b5.png"
+  }
+]);
